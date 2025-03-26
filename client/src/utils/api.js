88ba37e05b2ba6c87283f8f6331adbda8372b92b
zoherr <workforzoher@gmail.com>
2025-03-26 20:36:31 +0530

    ip change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "http://192.168.0.101:8000/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
